d2d682a3443fdcd85e265f89b04ff831f7c709e5
Lexi Quint <lexiqt@gmail.com>
2019-01-28 07:30:01 -0800

    added catchall for 404 pages - working on dev server but not with heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-app = express();
+
+const app = express();
+
 app.use(serveStatic(path.join(__dirname, "../dist")));
-const port = process.env.PORT || 80;
+
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "../dist/index.html");
+});
+
+const port = process.env.PORT || 8000;
 app.listen(port);
