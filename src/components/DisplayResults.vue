bead97e2c8886356ea5ced846d4c4377f902ae8d
Lexi Quint <lexiqt@gmail.com>
2019-01-28 09:22:07 -0800

    added components for search and search result view. Tested adding in a fake json object - but not working currently

---
+++ src/components/DisplayResults.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="(book, index) in itemList" :key="book.id">
+      <h1>{{ index }}</h1>
+      <!-- placeholder -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayResults",
+  props: {
+    itemList: Array
+  }
+};
+</script>
+
+<style>
+</style>
