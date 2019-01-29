ee9f48d8a0dc7292abbd40e46c63680c38327a25
Lexi Quint <lexiqt@gmail.com>
2019-01-28 22:46:02 -0800

    added content to bookdetail and basic styling

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>{{ itemInfo.title }}</h1>
+  <div class="section-detail">
+    <div>
+      <img :src="itemInfo.imageLinks.thumbnail" :alt="getTitle">
+    </div>
+    <h1>{{ getTitle }}</h1>
+    <h2>By{{ getAllAuthors }}</h2>
+    <h4>A Short Description</h4>
+    <p>The book was published in {{ getDate }} by {{ getPublisher }}. It was written by{{ getAllAuthors }}.</p>
+    <p>{{ getDescription }}</p>
+  </div>
 </template>
 
 <script>
@@ -7,9 +16,40 @@ export default {
   name: "BookDetail",
   props: {
     itemInfo: {}
+  },
+  computed: {
+    // Adding computed getters for the information.
+    // This will allow me to manipulate the info as I need more easily and seperate from the view layer
+    getTitle() {
+      let title = this.itemInfo.title;
+      return title;
+    },
+    getAllAuthors() {
+      let allAuthors = "";
+      for (let [id, value] of Object.entries(this.itemInfo.authors)) {
+        allAuthors += " " + value;
+      }
+      return allAuthors;
+    },
+    getDate() {
+      let date = this.itemInfo.publishedDate;
+      return date;
+    },
+    getPublisher() {
+      let publisher = this.itemInfo.publisher;
+      return publisher;
+    },
+    getDescription() {
+      let description = this.itemInfo.description;
+      return description;
+    }
   }
 };
 </script>
 
 <style>
+.section-detail {
+  align-content: justify;
+  margin: 0 50px;
+}
 </style>
